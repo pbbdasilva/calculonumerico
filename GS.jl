f473849c479f1c65928441ef107ba832ee1ff7ce
pbbdasilva <pbbdasilva@gmail.com>
2020-09-08 00:01:26 -0300

    trabalhos de calculo numerico no IME usando julia :)

---
+++ GS.jl
@@ -0,0 +1,31 @@
+using LinearAlgebra
+function solvegs(n, w)
+    d = repeat([2], n)
+    dl = repeat([-1], n-1)
+    A = Tridiagonal(dl, d, dl)
+    b = zeros(n)
+    b[1] = 1
+    # solucao exata
+    x = [1-k/(n+1) for k = 1:n]
+    Xk0 = zeros(1, n)
+    Xk1 = zeros(1, n)
+    Dk1 = max(abs(Xk1 - Xk0))
+    iters = 0
+    TOL = 10^(-8)
+    while Dk1 > TOL
+        if iter > 9999
+            println("fracassou")
+            return nothing
+        end
+        for i = 1:n
+            Xk0[i] = Xk1[i]
+            Xk1[i] = (b[i] - dot(A[i,:],Xk0) + A[i,i])/A[i,i]
+
+    end
+    D = Diagonal(d)
+    L = LowerTriangular(A) - D
+    U = UpperTriangular(A) - D
+    M = (1/w)*D + L
+    N = ((1-w)/(w))*D - U
+
+end
---
+++ createb.jl
@@ -0,0 +1,24 @@
+import LinearAlgebra
+function calculateG(n)
+    d = repeat([2], n)
+    dl = repeat([-1], n-1)
+    resultados = Array{Float64}(undef, 20)
+    w = collect(0.1:0.1:2)
+    A = Tridiagonal(dl, d, dl)
+    D = Diagonal(d)
+    L = LowerTriangular(A) - D
+    U = UpperTriangular(A) - D
+    i = 1
+    for w_k in w
+        M = (1/w_k)*D + L
+        N = ((1-w_k)/(w_k))*D - U
+        M = Matrix(M)
+        N = Matrix(N)
+        G = M \ N
+        F = svd(G)
+        resultados[i] = F.S[1]
+        i += 1
+    end
+    index = argmin(resultados)
+    return w[index]
+end
